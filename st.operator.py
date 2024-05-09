17ca7d2e48ec6da680a5a36119d3a67b6c5e4bbb
Hira2003 <hirafuyu2003@gmail.com>
2024-05-09 19:15:08 +0100

    Update st.operator.py

--- st.operator.py
+++ st.operator.py
@@ -8,19 +8,20 @@ try:
   j = int(st.text_input('num2=',""))
 except:
     st.write("fill in numbers first")
-c1, c2 = st.columns(2)
+c1, c2, c3, c4 = st.columns(2)
 with c1:
   if(st.button('addition')):
     st.write(j,'+',i,'=',j+i)
     st.write(i,'+',j,'=',i+j)
-
+with c2:
   if(st.button('substraction')):
     st.write(j,'-',i,'=',j-i)
     st.write(i,'-',j,'=',i-j)
-with c2:
+with c3:
  if(st.button('multiplication')):
     st.write(j,'*',i,'=',j*i)
     st.write(i,'*',j,'=',i*j)
+with c4:
  if(st.button('division')):
     st.write(j,'/',i,'=',j/i)
     st.write(i,'/',j,'=',i/j)
